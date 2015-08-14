2e874a863d10e89db6ba76bba60f09912e04e10b
Johannes Wienke <languitar@semipol.de>
2015-08-14 18:35:20 +0200

    Add a setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='autosuspend',
+    version='0.1',
+    description='Automatically suspends a Linux server in case of no activity',
+    author='Johannes Wienke',
+    author_email='languitar@semipol.de',
+
+    zip_safe=False,
+
+    install_requires=[
+        'psutil'
+    ],
+
+    scripts=[
+        'autosuspend.py'
+    ],
+    data_files=[
+        ('etc', ['autosuspend.conf'])
+    ]
+)
