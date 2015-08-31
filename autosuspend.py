2d558579c1584e1850e2e03e27db166c68c193d9
Johannes Wienke <languitar@semipol.de>
2015-08-31 22:51:47 +0200

    Fix process check
    Return None instead of False in case no process is running. This is the
    required protocol.

--- autosuspend.py
+++ autosuspend.py
@@ -218,7 +218,7 @@ class Processes(Check):
                         return 'Process {} is running'.format(name)
             except psutil.NoSuchProcess:
                 pass
-        return False
+        return None
 
 
 class ActiveConnection(Check):
