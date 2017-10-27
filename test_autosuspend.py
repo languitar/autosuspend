1335e9883def881dae412a4a5a86d9c8d6c0f2aa
Johannes Wienke <languitar@semipol.de>
2017-10-27 10:59:10 +0200

    Improve the SMB test
    also test the detection of connections

--- test_autosuspend.py
+++ test_autosuspend.py
@@ -14,3 +14,13 @@ class TestSmb(object):
         monkeypatch.setattr(subprocess, 'check_output', return_data)
 
         assert autosuspend.Smb('foo').check() is None
+
+    def test_with_connections(self, monkeypatch):
+        def return_data(*args, **kwargs):
+            with open(os.path.join(os.path.dirname(__file__), 'test_data',
+                                   'smbstatus_with_connections'), 'rb') as f:
+                return f.read()
+        monkeypatch.setattr(subprocess, 'check_output', return_data)
+
+        assert autosuspend.Smb('foo').check() is not None
+        assert len(autosuspend.Smb('foo').check().splitlines()) == 3
