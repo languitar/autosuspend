b0f58fc648d63ffc71181759b462f756d6a8f45f
Johannes Wienke <languitar@semipol.de>
2018-05-03 12:04:43 +0200

    Use tmpdir fixture for integration tests

--- test_integration.py
+++ test_integration.py
@@ -1,8 +1,6 @@
 import os
 import os.path
 
-import pytest
-
 import autosuspend
 
 
@@ -12,35 +10,36 @@ SUSPENSION_FILE = 'would_suspend'
 WOKE_UP_FILE = 'test-woke-up'
 
 
-@pytest.fixture
-def suspension_file():
-    try:
-        os.remove(SUSPENSION_FILE)
-    except OSError as error:
-        pass
-
-    class SuspensionFileFixture(object):
-
-        def exists(self):
-            return os.path.exists(SUSPENSION_FILE)
-
-    yield SuspensionFileFixture()
-
-    try:
-        os.remove(SUSPENSION_FILE)
-    except OSError as error:
-        pass
+def configure_config(config, tmpdir):
+    out_path = tmpdir.join(config)
+    with open(os.path.join(ROOT, 'test_data', config), 'r') as in_config:
+        with out_path.open('w') as out_config:
+            out_config.write(in_config.read().replace('@TMPDIR@',
+                                                      tmpdir.strpath))
+    return out_path
 
 
-def test_no_suspend_if_matching(suspension_file):
+def test_no_suspend_if_matching(tmpdir):
     autosuspend.main([
         '-c',
-        os.path.join(ROOT, 'test_data', 'dont_suspend.conf'),
+        configure_config('dont_suspend.conf', tmpdir).strpath,
         '-r',
         '10',
         '-l'])
 
-    assert not suspension_file.exists()
+    assert not tmpdir.join(SUSPENSION_FILE).check()
+
+
+def test_suspend(tmpdir):
+    autosuspend.main([
+        '-c',
+        configure_config('would_suspend.conf', tmpdir).strpath,
+        '-r',
+        '10',
+        '-l'])
+
+    assert tmpdir.join(SUSPENSION_FILE).check()
+
 
 
 def test_suspend(suspension_file):
@@ -54,18 +53,12 @@ def test_suspend(suspension_file):
     assert suspension_file.exists()
 
 
-def test_woke_up_file_removed():
-    try:
-        open(WOKE_UP_FILE, 'a').close()
-        autosuspend.main([
-            '-c',
-            os.path.join(ROOT, 'test_data', 'dont_suspend.conf'),
-            '-r',
-            '5',
-            '-l'])
-        assert not os.path.exists(WOKE_UP_FILE)
-    finally:
-        try:
-            os.remove(WOKE_UP_FILE)
-        except OSError as error:
-            pass
+def test_woke_up_file_removed(tmpdir):
+    tmpdir.join(WOKE_UP_FILE).ensure()
+    autosuspend.main([
+        '-c',
+        configure_config('dont_suspend.conf', tmpdir).strpath,
+        '-r',
+        '5',
+        '-l'])
+    assert not tmpdir.join(WOKE_UP_FILE).check()
