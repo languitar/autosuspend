b9cd871183db51ae07d9d88b447e9c9110f5dcce
Johannes Wienke <languitar@semipol.de>
2022-08-21 12:50:47 +0200

    refactor: extra Periodic wakeup in stub module
    This module will collect stub tests that are used as helpers.

---
+++ src/autosuspend/checks/stub.py
@@ -0,0 +1,27 @@
+import configparser
+from datetime import datetime, timedelta
+from typing import Optional
+
+from . import ConfigurationError, Wakeup
+
+
+class Periodic(Wakeup):
+    """Always indicates a wake up after a specified delta of time from now on.
+
+    Use this to periodically wake up a system.
+    """
+
+    @classmethod
+    def create(cls, name: str, config: configparser.SectionProxy) -> "Periodic":
+        try:
+            kwargs = {config["unit"]: float(config["value"])}
+            return cls(name, timedelta(**kwargs))
+        except (ValueError, KeyError, TypeError) as error:
+            raise ConfigurationError(str(error))
+
+    def __init__(self, name: str, delta: timedelta) -> None:
+        Wakeup.__init__(self, name)
+        self._delta = delta
+
+    def check(self, timestamp: datetime) -> Optional[datetime]:
+        return timestamp + self._delta
--- src/autosuspend/checks/wakeup.py
+++ src/autosuspend/checks/wakeup.py
@@ -1,15 +1,11 @@
-import configparser  # noqa
 from contextlib import suppress
-from datetime import datetime, timedelta
-from typing import Optional
-
-from . import ConfigurationError, Wakeup
 
 
 # isort: off
 
-from .linux import File  # noqa
 from .command import CommandWakeup as Command  # noqa
+from .linux import File  # noqa
+from .stub import Periodic  # noqa
 
 with suppress(ModuleNotFoundError):
     from .ical import Calendar  # noqa
@@ -20,25 +16,3 @@ with suppress(ModuleNotFoundError):
     from .systemd import SystemdTimer  # noqa
 
 # isort: on
-
-
-class Periodic(Wakeup):
-    """Always indicates a wake up after a specified delta of time from now on.
-
-    Use this to periodically wake up a system.
-    """
-
-    @classmethod
-    def create(cls, name: str, config: configparser.SectionProxy) -> "Periodic":
-        try:
-            kwargs = {config["unit"]: float(config["value"])}
-            return cls(name, timedelta(**kwargs))
-        except (ValueError, KeyError, TypeError) as error:
-            raise ConfigurationError(str(error))
-
-    def __init__(self, name: str, delta: timedelta) -> None:
-        Wakeup.__init__(self, name)
-        self._delta = delta
-
-    def check(self, timestamp: datetime) -> Optional[datetime]:
-        return timestamp + self._delta
